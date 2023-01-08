19160204a33593ce915b8e79b5d1461c33e2329e
SCL <76017102+SimClb@users.noreply.github.com>
2023-01-08 01:16:01 +0100

    concat finished

---
+++ epEau02.py
@@ -0,0 +1,29 @@
+import sys 
+import OurLib
+import colors
+
+
+# functions 
+
+def concat(list, separator):
+
+    for i in list: 
+        print(i, end=separator)
+
+
+
+
+# error management 
+
+if OurLib.lenCounter(sys.argv) < 3:
+    print('error.')
+    exit()
+
+# exe 
+
+s = (OurLib.lenCounter(sys.argv) -1)
+f = (OurLib.lenCounter(sys.argv) -2)
+
+concat(sys.argv[1:f], sys.argv[s])
+
+# finished
