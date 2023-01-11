540913c65fb13df3fa363130d35bb318d4be743e
SCL <76017102+SimClb@users.noreply.github.com>
2023-01-11 23:31:13 +0100

    melanger deux tab finished

---
+++ epAir08.py
@@ -0,0 +1,65 @@
+import sys 
+import OurLib
+import colors 
+# Mélanger deux tableaux triés ou non d'ailleurs 
+
+index = 0
+
+# funciton 
+
+def blenderList(list):
+
+    list1 = list[:index]
+    print(list1)
+    list2 = list[index + 1:]
+    print(list2)
+    listFusion = []
+
+    for i in list1:
+        listFusion.append(int(i))
+    
+    for j in list2:
+        listFusion.append(int(j))
+
+    for i in OurLib.bubbleSort(listFusion):
+        print(i, end=' ')
+
+# error management 
+
+counter = 0 
+
+if OurLib.lenCounter(sys.argv[1:]) < 4:
+    print('error.')
+    exit()
+
+# check if the word fusion is well in the list + find his position 
+for i in sys.argv[1:]:
+    if i == 'fusion': 
+        index += counter
+
+    counter += 1
+
+
+# if index = 0 ==> fusion is not in the list -> error
+if index == 0:
+    print('error.')
+    exit()
+
+# check if all the object in the list1 are numeric 
+for i in sys.argv[1:index]:
+    if not OurLib.isNumericHomeMade(i):
+        print('error.')
+        exit()
+
+# check if all the object in the list2 are numeric 
+for i in sys.argv[index + 2:]:
+    if not OurLib.isNumericHomeMade(i):
+        print('error.')
+        exit()
+
+# exe 
+
+print(index)
+blenderList(sys.argv[1:])
+
+# finished 
